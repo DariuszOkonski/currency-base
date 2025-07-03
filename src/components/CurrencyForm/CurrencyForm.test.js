a2a63724433791ef285ca674d7986ef379ae07f6
Dariusz Okoński <darek200180@gmail.com>
2025-07-03 07:27:58 +0200

    add data-testid to input and select elements

---
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import CurrencyForm from './CurrencyForm';
+import userEvent from '@testing-library/user-event';
+
+describe('Component CurrencyForm', () => {
+  it('should render without crashing', () => {
+    render(<CurrencyForm action={() => {}} />);
+  });
+
+  it('should run action callback with proper data on form submit', () => {
+    const action = jest.fn();
+    render(<CurrencyForm action={action} />);
+
+    const submitButton = screen.getByText('Convert');
+
+    userEvent.click(submitButton);
+
+    expect(action).toHaveBeenCalledTimes(1);
+  });
+});
